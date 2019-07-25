4fc2089bd915faedc765145f94f1c17a26b26f05
Takahiro Ishikawa <takahish.nil@gmail.com>
2019-07-25 16:31:23 +0900

    feat: Added etl processes jupyter notebook.
    Added etl processes jupyter notebook

--- download_customer_reviews.sh
+++ download_customer_reviews.sh
@@ -3,7 +3,3 @@
 # Download sample data from citus to load cstore.
 wget http://examples.citusdata.com/customer_reviews_1998.csv.gz
 wget http://examples.citusdata.com/customer_reviews_1999.csv.gz
-
-# Uncompress sample data.
-gzip -d customer_reviews_1998.csv.gz
-gzip -d customer_reviews_1999.csv.gz
